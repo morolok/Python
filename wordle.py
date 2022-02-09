81d25b9f0e94193ab2df9cdf0d5ecf2654fd933c
morolok <35529654+morolok@users.noreply.github.com>
2022-02-09 14:53:58 +0100

    Terminación de palabra y restricciones para Sexto

--- wordle.py
+++ wordle.py
@@ -1,31 +1,77 @@
+from math import remainder
+from os import remove
+from turtle import pos
+
+
 palabras = []
 posibles_palabras = []
 
-def extraer_palabras():
-    with open('diccionario.txt', 'r', encoding='utf-8') as f:
-        palabras_diccionario = f.readlines()
-        for p in palabras_diccionario:
-            pal = p.strip()
-            if(len(pal) == 5):
-                palabras.append(pal)
-    f.close()
 
-def condiciones_palabras():
-    # Filtrar las palabras que tengan o no letras y nos las quedamos
-    for pal in palabras:
-        if(('o' in pal or 'ó' in pal) and ('a' in pal or 'á' in pal) and ('v' in pal) and 
-        ('n' not in pal) and ('i' not in pal) and ('í' not in pal)):
-            posibles_palabras.append(pal)
-    print(len(posibles_palabras))
-    # Filtrar de las posibles las palabras con letras en la posición
-    for pal in posibles_palabras:
-        if(not pal[1] == 'o'):
-            posibles_palabras.remove(pal)
-        elif(not pal[1] == 'ó'):
-            posibles_palabras.remove(pal)
-    print(len(posibles_palabras))
-    print(posibles_palabras)
+def wordle_script():
+
+    def extraer_palabras():
+        with open('diccionario.txt', 'r', encoding='utf-8') as f:
+            palabras_diccionario = f.readlines()
+            for p in palabras_diccionario:
+                pal = p.strip()
+                if(len(pal) == 5):
+                    palabras.append(pal)
+        f.close()
+
+    def condiciones_palabras():
+        # Filtrar las palabras que tengan las letras amarillas o verdes y nos las quedamos
+        for pal in palabras:
+            if(((pal[4] == 'o') or (pal[4] == 'ó')) and ((pal[1] == 'e') or (pal[1] == 'é')) and (pal[0] == 's')):
+                posibles_palabras.append(pal)
+        print(len(posibles_palabras))
+
+        # Eliminar las palabras que no tengan las letras en gris
+        for i in range(15):
+            for pal in posibles_palabras:
+                if('n' in pal):
+                    posibles_palabras.remove(pal)
+                elif('v' in pal):
+                    posibles_palabras.remove(pal)
+                elif('i' in pal):
+                    posibles_palabras.remove(pal)
+                elif('í' in pal):
+                    posibles_palabras.remove(pal)
+                elif('a' in pal):
+                    posibles_palabras.remove(pal)
+                elif('á' in pal):
+                    posibles_palabras.remove(pal)
+                elif('q' in pal):
+                    posibles_palabras.remove(pal)
+                elif('u' in pal):
+                    posibles_palabras.remove(pal)
+                elif('ú' in pal):
+                    posibles_palabras.remove(pal)
+                elif('d' in pal):
+                    posibles_palabras.remove(pal)
+                elif('l' in pal):
+                    posibles_palabras.remove(pal)
+        print(len(posibles_palabras))
+        
+        # Eliminar las palabras que tengan letras en posicion amarilla
+        for i in range(5):
+            pass
+            for pal in posibles_palabras:
+                pass
+        print(len(posibles_palabras))
+        print(posibles_palabras)
+    
+    def termina_palabra():
+        for pal in palabras:
+            if(pal.endswith('risa')):
+                print(pal)
+        
+
+    extraer_palabras()
+    condiciones_palabras()
+    #termina_palabra()
 
 
-extraer_palabras()
-condiciones_palabras()
+def wordle_web_scraping():
+    pass
+
+wordle_script()
